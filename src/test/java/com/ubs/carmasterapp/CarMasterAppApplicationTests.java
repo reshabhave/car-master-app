5a77d549b60335a8bcb43c9aeed60fdae14edb26
unknown <resha.narayan.bhave@accenture.com>
2020-02-24 20:13:04 +0530

    Junits updated

--- src/test/java/com/ubs/carmasterapp/CarMasterAppApplicationTests.java
+++ src/test/java/com/ubs/carmasterapp/CarMasterAppApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -15,11 +16,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.validation.constraints.NotNull;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
@@ -31,10 +30,13 @@ class CarMasterAppApplicationTests {
 	@LocalServerPort()
 	int randomServerPort;
 
+	@Value("${car.service.URL}")
+	private String carServiceURL;
+
 	@Test
 	public void testCreateCarSuccess() throws URISyntaxException
 	{
-		final String baseUrl = "http://localhost:8080/carmaster-example/api/cars";
+		final String baseUrl = carServiceURL;
 		URI uri = new URI(baseUrl);
 		Car carObj = new Car("Honda", 0, "Amaze", 2014, "PETROL");
 		HttpHeaders headers = new HttpHeaders();
@@ -47,7 +49,7 @@ class CarMasterAppApplicationTests {
 	@Test
 	public void testCreateCarNameValidation() throws URISyntaxException
 	{
-		final String baseUrl = "http://localhost:8080/carmaster-example/api/cars";
+		final String baseUrl = carServiceURL;
 		URI uri = new URI(baseUrl);
 		Car carObj = new Car("Maruti Suzuki", 0, "Alto", 2012, "PETROL");
 		HttpHeaders headers = new HttpHeaders();
@@ -62,7 +64,7 @@ class CarMasterAppApplicationTests {
 	@Test
 	public void testCreateCarAgeValidation() throws URISyntaxException
 	{
-		final String baseUrl = "http://localhost:8080/carmaster-example/api/cars";
+		final String baseUrl = carServiceURL;
 		URI uri = new URI(baseUrl);
 		Car carObj = new Car("Maruti", 0, "Alto", 2004, "PETROL");
 		HttpHeaders headers = new HttpHeaders();
@@ -77,7 +79,7 @@ class CarMasterAppApplicationTests {
 	@Test
 	public void testGetCarById() throws URISyntaxException
 	{
-		final String baseUrl = "http://localhost:8080/carmaster-example/api/cars/1";
+		final String baseUrl = carServiceURL + "/1";
 		URI uri = new URI(baseUrl);
 
 		ResponseEntity<Car> result = this.restTemplate.getForEntity(uri, Car.class);
@@ -88,7 +90,7 @@ class CarMasterAppApplicationTests {
 	@Test
 	public void testGetAllCars() throws URISyntaxException
 	{
-		final String baseUrl = "http://localhost:8080/carmaster-example/api/cars";
+		final String baseUrl = carServiceURL;
 		URI uri = new URI(baseUrl);
 
 		ResponseEntity<List> result = this.restTemplate.getForEntity(uri, List.class);
@@ -100,7 +102,7 @@ class CarMasterAppApplicationTests {
 	@Test
 	public void testUpdateCar() throws URISyntaxException
 	{
-		final String baseUrl = "http://localhost:8080/carmaster-example/api/cars/1";
+		final String baseUrl = carServiceURL + "/1";
 		URI uri = new URI(baseUrl);
 
 		Car carObj = new Car("Honda", 900000, "Amaze", 2014, "PETROL");
@@ -115,7 +117,7 @@ class CarMasterAppApplicationTests {
 	@Test
 	public void testDeleteCar() throws URISyntaxException
 	{
-		final String baseUrl = "http://localhost:8080/carmaster-example/api/cars/1";
+		final String baseUrl = carServiceURL + "/1";
 		URI uri = new URI(baseUrl);
 
 		Car carObj = new Car("Honda", 900000, "Amaze", 2014, "PETROL");
